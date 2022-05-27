1c6226021916e6d0153eccf1b30f92627bad7988
Samuel Sau <samuel51@usf.edu>
2022-05-26 20:39:28 -0400

    added pressure, icon, add logic script.js, fix styles

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-import API_KEY from "./config.js";
+import API_KEY from './config.js';
 
 async function requestWeather(cityName) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`)
-    const data = await response.json()
-    console.log(data)
-    
+	const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=imperial`);
+	const data = await response.json();
+	return data;
 }
 
-/* function getTemperature
-    //console.log(data.main.temp)
-    function getDescription
-    //console.log(data.weather[0].description)
-    function getHumidity
-    //console.log(data.main.temp)
-    function getWindSpeed
-    //console.log(data.wind.speed)
-    function getPressure
-    //console.log(data.main.pressure)
+function submitButton(){
+    const submission = document.querySelector('button');
 
-*/
+    submission.addEventListener('click', async () => {
 
-requestWeather("Orlando");
+    const userInput = document.querySelector('input').value;
+    const data = await requestWeather(userInput);
+    renderCity(data);
+});
+}
+
+function renderCity(data) {
+	const { name } = data;
+	const { description, icon } = data.weather[0];
+	const { temp, pressure, humidity } = data.main;
+	const { speed } = data.wind;
+	const { lon, lat } = data.coord;
+
+	console.log(name, description, temp, pressure, humidity, speed, lon, lat)
+	document.querySelector('.city').innerHTML = `${name}`;
+	document.querySelector('.description').innerText = description;
+	document.querySelector('img').src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+	document.querySelector('.temp').innerText = `${temp}℉`;
+	document.querySelector('.pressure').innerText = `Pressure: ${pressure}`;
+	document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+	document.querySelector('.wind').innerText = `Wind: ${speed}mph`;
+	document.querySelector('.lon').innerText = `Longitude: ${lon}`;
+	document.querySelector('.lat').innerText = `Latitude: ${lat}`;
+}
+
+submitButton();
